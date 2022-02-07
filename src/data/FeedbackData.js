75c943b03e899c67b9e0cdde2e92cd248b77a791
Anthony Suarez <antsuarez305@gmail.com>
2022-02-06 18:06:54 -0600

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'This is a test',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'This is another test',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'The last test',
+  },
+]
+
+export default FeedbackData
